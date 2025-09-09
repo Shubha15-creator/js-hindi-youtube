428c6dfd62b0cb5d850b8394a97b9fb0df9b8809
Shubha15-creator <shubhanshisendhiya15@gmail.com>
2025-09-09 20:55:59 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = "24569"
+let accountEmail = "shubh@gmail.com"
+var accountPassword = "123456"
+accountCity = "Sagar"
+
+accountEmail="abc@gmail.com"
+accountPassword="5698"
+accountCity="jabalpur"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"user  strict" // treat all js code as newer version
+//alert(3 + 3) // we are using node js not browser
+
+
+let name = "shubh"
+let age = "18"
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//boolean => true/false
+//null => standalone value
+// undefined => value is not assigned
+//symbol => unique 
+//object
+console.log(typeof "shubh")
